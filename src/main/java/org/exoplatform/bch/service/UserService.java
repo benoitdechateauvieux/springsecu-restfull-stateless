d38cd53b1c085bdc8a1a84bd2cee53d9becdffdf
Benoit de Chateauvieux <bdechateauvieux@may127.l.exoplatform.vn>
2015-04-08 13:40:00 +0700

    test with explicit session creation

--- src/main/java/org/exoplatform/bch/service/UserService.java
+++ src/main/java/org/exoplatform/bch/service/UserService.java
@@ -1,10 +1,14 @@
 package org.exoplatform.bch.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.util.Arrays;
 import java.util.List;
@@ -18,7 +22,10 @@ public class UserService {
 
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
-    public List<User> getUsers() {
+    public List<User> getUsers(@Context HttpServletRequest request) {
+        //Someone creates a session
+        request.getSession(true);
+
         return Arrays.asList(new User("Benoit"));
     }
 }
